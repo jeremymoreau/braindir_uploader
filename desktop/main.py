3c91aaa4cab3078bb8c926be15642906a0b1ff5e
jeremy_moreau <jeremy.moreau@gmail.com>
2014-05-10 21:03:58 -0400

    add extract .tar.gz

--- desktop/main.py
+++ desktop/main.py
@@ -2,6 +2,7 @@ import paramiko
 from scp import SCPClient
 import os
 import tarfile
+from time import sleep
 
 ##### General variables
 local_path = os.path.realpath('.')
@@ -35,9 +36,7 @@ ssh.load_host_keys(hostkey_file)
 #scp = SCPClient(ssh.get_transport())
 
 
-##### tar and gzip the dir_to_upload / extract .tar.gz archive
-# functions and counter for progress bar
-
+##### General Functions
 def get_size(dir_path):
 	"""get the size of dir at dir_path recursively"""
 	total_size = 0
@@ -47,16 +46,18 @@ def get_size(dir_path):
 			total_size += os.path.getsize(fp)
 	return total_size
 
+
+##### tar and gzip the dir_to_upload / extract .tar.gz archive
 def compress(dir_path):
 	"""adds dir at dir_path to tmp_archive.tar.gz in ./files/ and tracks operation progress"""
 	# get basename of the directory to compress
 	root_path = os.path.basename(dir_path)
 	
 	# set progress counter to 0
-	progress_counter = 0
+	compress_progress_counter = 0
 	
 	# create empty archive in the ./files/ directory
-	tar = tarfile.open(os.path.join(local_path,'files','tmp_archive.tar.gz'),'w:gz')
+	tar = tarfile.open(os.path.join(local_path,'files','tmp_archive.tar.gz'), 'w:gz')
 	
 	# recursively add all files in the dir at dir_path to the empty archive
 	# also track progress (which is why os.walk and convoluted code is used to loop over files)
@@ -64,17 +65,49 @@ def compress(dir_path):
 		for f in filenames:
 			file_path = os.path.join(dirpath, f)
 
+			# get the relative path of the file to add
 			path_split = file_path.partition(root_path)
 			name_for_file = path_split[1] + path_split[2]
-
-			tar.addfile(tarfile.TarInfo(name_for_file), file(file_path))
 			
+			# generate tarinfo object for the file to add using the relative path
+			# this avoids having the whole directory tree added to the archive
+			file_tarinfo = tar.gettarinfo(name = file_path, arcname = name_for_file)
 			
-			# track progress of compression (!!! update the print to GUI display later !!!)	
-			progress_counter += os.path.getsize(file_path)
-			print(progress_counter)
+			# add file to archive
+			tar.addfile(file_tarinfo, file(file_path))
+			
+			# get total size of directory to compress
+			dir_total_size = get_size(dir_path)
+			#print(dir_total_size)
+			
+			# track progress of compression	
+			compress_progress_counter += os.path.getsize(file_path)
+			#print(compress_progress_counter) # !!! update the print to GUI display later !!!
+	
 	tar.close()
 	
+def extract(archive_path):
+	"""extract the archive at archive_path and save the extracted files at path_to_save"""
+	# archive_size = os.path.getsize(archive_path)
+	
+	# open tar.gz archive
+	tar = tarfile.open(os.path.join(local_path,'files','tmp_archive.tar.gz'),'r:gz')
+	
+	# get total size of uncompressed files
+	size_of_extracted_archive = 0
+	for f in tar.getmembers():
+		size_of_extracted_archive += f.size
+	print(size_of_extracted_archive)
+	
+	# track progress of extraction
+	extract_progress_counter = 0
+	# while (extract_progress_counter != size_of_extracted_archive)
+	#	extract_progress_counter = 
+	
+	# extract all files 
+	tar.extractall('./files/')
+	tar.close()
+		
 
 ##### Encrypt the dir_to_upload with password_for_dir / decrypt
 
@@ -84,6 +117,10 @@ def compress(dir_path):
 
 
 ##### Testing
-print(get_size(dir_to_upload))
 
+## compress
+print(get_size(dir_to_upload))
 compress(dir_to_upload)
+
+## extract
+extract(os.path.join(local_path,'files','tmp_archive.tar.gz'))
