0d049bdb40704938f1c3d8b2646f6f11ee341838
jeremy_moreau <jeremy.moreau@gmail.com>
2014-05-23 18:39:39 -0400

    add Bootstrap progress bar updated by python backend via sijax

--- desktop/gui.py
+++ desktop/gui.py
@@ -1,28 +1,35 @@
+# -*- coding: utf-8 -*-
+
 import main
 import os
-from flask import Flask, render_template
-from flask import request, redirect
+from flask import Flask, g, render_template, request, redirect
+import flask_sijax
 
-LOCAL_FILE_STORAGE_DIR = os.path.join(os.path.realpath('.'), 'files')
-print(LOCAL_FILE_STORAGE_DIR)
+progress_bar_value = '95%'
 
+# LOCAL_FILE_STORAGE_DIR = os.path.join(os.path.realpath('.'), 'files')
+# print(LOCAL_FILE_STORAGE_DIR)
+
+# initialise app
 app = Flask(__name__)
 
-@app.route('/')
-def view_1():
-	return render_template('view_1.html')
+# config for flask-sijax
+app.config["SIJAX_STATIC_PATH"] = os.path.join(main.local_path, 'static', 'js', 'sijax')
+flask_sijax.Sijax(app)
 
-@app.route('/view_1_actions', methods = ['POST'])
-def view_1_actions():
-    dir_to_upload = request.files.getlist('dir_to_upload')
-    #filename = dir_to_upload.filename
-    #dir_to_upload.save(os.path.join(LOCAL_FILE_STORAGE_DIR, filename))
-    print(dir_to_upload)
-    return redirect('/')
-    
-# @app.route('/emails')
-# def emails():
-#     return render_template('emails.html', email_addresses=email_addresses)
+class SijaxHandler(object):
+	@staticmethod
+	def set_progress(obj_response):
+		obj_response.css('.progress-bar', 'width', progress_bar_value)
+		obj_response.html('.progress-bar-label',progress_bar_value)
+
+@flask_sijax.route(app, '/')
+def index():
+	if g.sijax.is_sijax_request:
+		g.sijax.register_object(SijaxHandler)
+		return g.sijax.process_request()
+	
+	return render_template('view_1.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
